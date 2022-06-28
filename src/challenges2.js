2b63d8890687308bd571262867a8f3feec120488
Francisco Costa <francisco100eg@gmail.com>
2022-06-28 07:47:05 -0300

    Resolução Desafio Bonus 3

--- src/challenges2.js
+++ src/challenges2.js
@@ -49,9 +49,22 @@ function triangleCheck(lineA,lineB,lineC) {
 }
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(string) {
+  soma = 0;
+  numeros = string.match(/\d+/g);
+  let numerosInt = numeros.map(Number);
+  for(let i = 0; i<numerosInt.length;i+=1){
+    soma += numerosInt[i]
+  }
+  if (soma === 1){
+    return soma + " copo de água"
+  }else{
+    return soma + " copos de água"
+  }
 }
+console.log(hydrate('1 cachaça, 5 cervejas e 1 copo de vinho'))
+
+
 
 module.exports = {
   generatePhoneNumber,
