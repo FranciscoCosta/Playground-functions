dd2bedd8d6cc462b725858266660aff990c3fad5
Francisco Costa <francisco100eg@gmail.com>
2022-06-28 07:11:25 -0300

    Resolução Desafio Bonus 1

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,38 @@
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
-}
+function generatePhoneNumber(array) {
+  let a = array.length;
+  cont = 0;
+  contMax = 0;
+  final =""
+  if(a!=11){
+    return "Array com tamanho incorreto."
+  }else{
+    for(let i=0;i<a;i+=1){
+      if(array[i]<0 || array[i]>=10) {
+        return "não é possível gerar um número de telefone com esses valores"
+      }else{
+      for (z in array){
+        if(array[i]==array[z]){
+          cont +=1;
+        }if (cont > contMax){
+          contMax = cont;
+        }
+      }
+      cont = 0
+    }if(contMax >= 3){
+      return "não é possível gerar um número de telefone com esses valores"
+    }
+    }
+  }
+  array.unshift("(");
+  array.splice(3,0,")")
+  array.splice(4,0," ")
+  array.splice(10,0,"-")
+  final = array.join("")
+  return final
+  }
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 5, 7, 8, 9, 0, 10]))
+
 
 // Desafio 12
 function triangleCheck() {
